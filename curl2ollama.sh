a070fd3508f727ce331d1d00c66c3f3faa4832e7
caimandepamer <rafael.campoverde.mariscal@gmail.com>
2024-05-27 12:14:39 +0200

    Create curl2ollama.sh

---
+++ curl2ollama.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Expected: 
+#      - environment variable: OLLAMAHOST
+#	     - jq and curl installed
+# run: ./curl2ollama.sh "What you want to ask"
+
+
+QUERY=$1
+MSG=$(curl http://${OLLAMAHOST}:11434/api/chat -d '{
+  "model": "llama3",
+  "stream": false,
+  "messages": [
+    { "role": "user", "content": "'"${QUERY}"'" }
+  ]
+}' 2>/dev/null)
+MSG2=$(echo $MSG | jq .message | jq .content|cat ); 
+echo "${MSG@E}"
